c3093b1ce2dcbf24410e7693e7a9be5f712f5191
guidanoli <guidanoli@hotmail.com>
2019-05-16 01:39:43 -0300

    queues and threads working well togheter

--- main.c
+++ main.c
@@ -6,12 +6,15 @@
 #include <unistd.h>
 #include <string.h>
 #include <errno.h>
+#include <sys/types.h> 
+#include <unistd.h>
+#include <sys/wait.h>
+#include <string.h>
 #include "queue.h"
 
 #define QTD_FILAS 3
 #define STR_PROGNAME_MAX 64
 #define PROG_FILENAME "prog"
-#define _DEBUG
 
 qhead filas[QTD_FILAS];
 qnode current_process = NULL;
@@ -19,10 +22,16 @@ qnode current_process = NULL;
 int main(int argc, char ** argv)
 {
   char c;
-  if( argc != 1 )
+  int status = 0;
+  pid_t pid;
+  
+  for( int i = 0 ; i < QTD_FILAS ; i++ )
   {
-    fprintf(stderr,"Número de argumentos inesperado.");
-    return EXIT_FAILURE;
+    if( qhead_create(filas,i+1) != 0 )
+    {
+      fprintf(stderr,"Não foi possível alocar espaço na memória.\n");
+      return EXIT_FAILURE;
+    }
   }
   
   while((c = fgetc(stdin)) == 'e')
@@ -30,19 +39,55 @@ int main(int argc, char ** argv)
     char prog[STR_PROGNAME_MAX] = "";
     int r[3];
     
-    if( scanf("xec %s (%d,%d,%d)\n",prog,r+0,r+1,r+2) != 4 )
+    if( scanf("xec %s (%d,%d,%d)\n",prog,r,r+1,r+2) != 4 )
     {
       fprintf(stderr,"String mal formatada.\n");
       return EXIT_FAILURE;
     }
     
-    // add program to ready list (?)
+    scanf(" ");
+    
     #ifdef _DEBUG
     printf("Carregado programa %s com rajadas %d, %d e %d.\n",prog,r[0],r[1],r[2]);
     #endif
     
-    scanf(" ");
+    if((pid = fork()) == 0)
+    {
+      sleep(1);
+      char r1[16];
+      char r2[16];
+      char r3[16];
+      sprintf(r1,"%d",r[0]);
+      sprintf(r2,"%d",r[1]);
+      sprintf(r3,"%d",r[2]);
+      execl(PROG_FILENAME,PROG_FILENAME,r1,r2,r3,(char *)NULL);
+    }
+    else
+    {
+      kill(pid,SIGSTOP);
+      qnode process;
+      printf("Capturei o processo de pid %d\n",pid);
+      if( qnode_create(&process,pid) != 0 )
+      {
+        fprintf(stderr,"Não foi possível alocar espaço na memória.\n");
+        return EXIT_FAILURE;
+      }
+      qhead_ins(filas[0],process);
+    }
     
   }
+  
+  while((current_process = qhead_rm(filas[0]))!=NULL)
+  {
+    pid = qnode_getid(current_process);
+    printf("Recuperado processo de pid %d\n",pid);
+    kill(pid,SIGCONT);
+    waitpid(pid,&status,0);
+    qnode_destroy(&current_process);
+  }
+  qhead_destroy(filas);
+  qhead_destroy(filas+1);
+  qhead_destroy(filas+2);
+  
 	return EXIT_SUCCESS;
 }
