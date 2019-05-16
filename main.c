9cb9f55eac821627fd5d0aec7f4ef89a37d57444
guidanoli <guidanoli@hotmail.com>
2019-05-15 21:23:47 -0300

    main working with stdin / qhead wip

--- main.c
+++ main.c
@@ -8,41 +8,26 @@
 #include <errno.h>
 
 #define STR_PROGNAME_MAX 64
+#define PROG_FILENAME "prog"
 #define _DEBUG
 
 int main(int argc, char ** argv)
 {
-  // Input
-  FILE * f;
   char c;
-  int line = 1;
-  
-  if( argc != 2 )
+  if( argc != 1 )
   {
-    fprintf(stderr,"Número de argumentos esperado: 1 Recebido: %d\n",argc-1);
+    fprintf(stderr,"Número de argumentos inesperado.");
     return EXIT_FAILURE;
   }
   
-  f = fopen(argv[1],"r");
-  if( f == NULL )
-  {
-    fprintf(stderr,"Não foi possível abrir '%s': %s\n",argv[1],strerror(errno));
-    return EXIT_FAILURE;
-  }
-  
-  while((c = fgetc(f)) != EOF )
+  while((c = fgetc(stdin)) == 'e')
   {
     char prog[STR_PROGNAME_MAX] = "";
     int r[3];
     
-    if( c != 'e' )
+    if( scanf("xec %s (%d,%d,%d)\n",prog,r+0,r+1,r+2) != 4 )
     {
-      fprintf(stderr,"Caracter inesperado na linha %d do arquivo '%s'. Esperado: 'e' Recebido: '%c'\n",line,argv[1],c);
-      return EXIT_FAILURE;
-    }
-    if( fscanf(f,"xec %s (%d,%d,%d)\n",prog,r+0,r+1,r+2) != 4 )
-    {
-      fprintf(stderr,"Linha %d do arquivo '%s' está mal formatada.\n",line,argv[1]);
+      fprintf(stderr,"String mal formatada.\n");
       return EXIT_FAILURE;
     }
     
@@ -51,9 +36,8 @@ int main(int argc, char ** argv)
     printf("Carregado programa %s com rajadas %d, %d e %d.\n",prog,r[0],r[1],r[2]);
     #endif
     
-    fscanf(f," ");
+    scanf(" ");
     
   }
-  fclose(f);
 	return EXIT_SUCCESS;
 }
