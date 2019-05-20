1e75525bbaddc8b9262c6bba655a94c9983edf31
guidanoli <guidanoli@hotmail.com>
2019-05-20 13:46:53 -0300

    Relatório, resultados e modificação no programa usuário

--- prog.c
+++ prog.c
@@ -7,23 +7,14 @@
   #include <unistd.h>
   #include <signal.h>
 
-  int locked = 1;
-
-  void handler(int signo) {
-    locked = 0;
-  }
-
   int main(int argc, char ** argv)
   {
     int ppid = getppid(), pid = getpid();
-    signal(SIGCONT,handler);
     #ifdef _DEBUG
     printf("Processo filho %d pronto para ser executado.\n",pid);
     #endif
     kill(ppid,SIGUSR2); // I am running!
-    while( locked );
-    signal(SIGCONT,SIG_DFL);
-    
+    sleep(1);
 	  for( int i = 1 ; i < argc ; i++ )
 	  {
 	    for( int j = 0 ; j < atoi(argv[i]) ; j++ )
