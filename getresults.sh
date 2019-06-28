8b79435c8a30e4decf12c12a32036ae52f5a6cd2
guidanoli <guidanoli@hotmail.com>
2019-06-28 01:24:06 -0300

    Deleted images -> Performance in ASCII tables
    Lighter, smarter and more elegant.
    Tables are generated according o the statistics sent to stdout
    by the simulador, caught by grep and converted to a single txt,
    that is then formated by a Python script.

--- getresults.sh
+++ getresults.sh
@@ -1,16 +1,19 @@
 #!/bin/bash
 OUTPUT="autoresults.txt"
+FORMATED_OUTPUT="results.txt"
 > $OUTPUT
 for ALG in NRU LRU NOVO
 do
-	echo $ALG | tee $OUTPUT
+	echo $ALG | tee -a $OUTPUT
 	for FILE in compilador compressor matriz simulador
 	do
-		echo $FILE | tee $OUTPUT
+		echo $FILE | tee -a $OUTPUT
 		for PAGE in 8 16 32
 		do
-			echo $PAGE | tee $OUTPUT
-			./sim-virtual $ALG $FILE.log $PAGE 16 | grep -oP "(faults|written): \K\d*" | tee $OUTPUT 
+			echo $PAGE | tee -a $OUTPUT
+			./sim-virtual $ALG $FILE.log $PAGE 16 | grep -oP "(faults|written): \K\d*" | tee -a $OUTPUT 
 		done
 	done
 done
+python3 autotable.py > $FORMATED_OUTPUT
+rm $OUTPUT
